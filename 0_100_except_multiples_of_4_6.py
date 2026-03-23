bd1ad863de34a0f51c20cc707e391e7d363a83c2
vyshnavi94944-svg <vyshnavi94944@gmail.com>
2026-03-23 14:23:00 +0530

    Add files via upload

---
+++ 0_100_except_multiples_of_4_6.py
@@ -0,0 +1,3 @@
+for i in range(0, 101):
+    if i % 4 != 0 and i % 6 != 0:
+      print(i)
---
+++ add_two_num(fun).py
@@ -0,0 +1,6 @@
+def add(a,b):
+    return a+b
+a=int(input("enter a:"))
+b=int(input("enter b:"))
+r=add(a,b)
+print("result:",r)
